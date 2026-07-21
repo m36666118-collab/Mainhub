1b1f53e25c489452a3b0bbd53f89a5852db31443
m36666118-collab <m36666118@gmail.com>
2026-07-20 19:43:15 -0600

    Update Kill.lua

--- Kill.lua
+++ Kill.lua
@@ -23,9 +23,9 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/imhen
 local window = library:AddWindow(title, {
     main_color = Color3.fromRGB(0, 0, 0),
     title_bar = {Color3.fromRGB(30, 30, 30), Color3.fromRGB(10, 10, 10)},
-    background = {Color3.fromRGB(5, 5, 5)},
+    background = {Color3.fromRGB(0, 0, 0)},
     background_transparency = 0,
-    min_size = Vector2.new(600, 580),
+    min_size = Vector2.new(500, 500),
     toggle_key = Enum.KeyCode.RightShift,
     can_resize = true,
 })
